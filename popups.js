3653932371b01053c9082a80de3692ec3c4a35e3
Jonas Anton Oestman <jonasbits@me.com>
2012-11-07 23:31:01 +0100

    Used HHPMod to fix everything (http://post.queensu.ca/~penstone/HHPMod/HHPMod_info.html)

---
+++ popups.js
@@ -0,0 +1,26 @@
+// Javascript Popups File written automatically by HHPMod Version 3.2.2
+// november 07, 2012, 11:14 
+
+popfont = "Arial,10,,plain"
+
+1memq8p = "You can place units on this terrain, and units can cross this terrain in the game. This includes terrain such as dirt, grass and concrete."
+bm9bal = "The amount of slots required for any unit/building in the game.  Certain units take up 4 unit slots, while other units will only take up a single unit slot."
+uy3ivq = "These include resources, critters, and power-ups."
+23rtfxo = "Units that are placed under the control of a player that send any units near."
+19dcpiq = "The Player can see terrain but cannot see any troops or activity in the area."
+definition_playerx = "Players are any units that are controlled by humans or the computer."
+1l9w5lx = "Current Player is the player that the trigger is evaluating the condition/action for."
+11bkybb = "Allies are any players that are allied with you, or are a part of the same force."
+27yjrcd = "Neutral Players are all players that are listed as Neutral under the player settings."
+definition_allplayers = "All Players are all of the players in the scenario."
+definition_forcex = "Forces are groups of players assigned to accomplish a similar set of goals.  Forces can be uniquely named for your convenience."
+k48sex = "Non Allied Victory Players are all of the players that do not have the allied victory check box chosen in their communications dialog."
+definition_players = "Any players and/or teams that have a trigger associated with them will be listed here.  This allows for quicker access to the trigger type that you want to modify."
+definition_conditions = "Any of the conditions that you create will be listed in this area with a checkmark beside the condition, allowing you to toggle its status on or off.  Also, you may select an individual condition and copy, delete, or modify its values."
+definition_uservalue = "User values are customizable to your settings, and they can also be modified at any time."
+definition_movedown = "This will move the currently selected item down in the list."
+definition_actions = "Any actions that you have defined will be listed in this area, and you will have the chance to re-order them so they occur in the proper order.  Each action will have a check mark next to them allowing you to toggle the status on and off.  You may also select an individual action and copy, delete, or modify the action."
+118bdi0 = "Leader Boards give all of the players in the scenario a chance to see where they rank.  Leader Boards are updated in real-time and keep track of statistics such as number of units a player controls, number of resources, and other things. "
+61gq_on = "1 game second at normal speed, is the equivalent of one real second."
+ivzpqz = "The players score is a value that is calculated from kills, razings, or another user-specified score-type."
+9ykvdl = "Trigger Tutorial: Step One\n (*) \nFrom the Scenario menu, select Triggers.\nThis dialog is the first screen of the Trigger wizard. The Trigger wizard walks you through the creation of a trigger, step-by-step. It confirms that you have entered all the proper information, and that the information is complete, before advancing to the next step. \nNote: Clicking on any of the buttons or fields in this graphic will display helpful information on its purpose.\nTo step through this tutorial, perform the actions specified under the Step sections, or simply click on the next page and previous links found at the bottom of each page.\nSTEP: Click on the New button.\nNext Section\nPrevious Section"
